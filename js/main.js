1b30ece6e00c80027c736699a115a45c63bf9020
Лайт <lightkasei@gmail.com>
2019-04-13 09:33:38 +0500

    calculate price & compile specs

--- js/main.js
+++ js/main.js
@@ -7,4 +7,37 @@ $(document).ready(function() {
 			$(this).attr('src', imgPath).fadeIn(200);
 		});
 	});
+
+	var itemSpecsHolder = $('.item-specs');
+	var	itemPriceHolder = $('.item-price_rur');
+	var	itemSpecs = '';
+	var	itemPrice = 0;
+
+	function numberWithSpaces(x) {
+	    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+	}
+
+	function calculatePrice() {
+		itemPrice = parseInt($('input[name=engine]:checked').val());
+		itemPrice +=  parseInt($('input[name=transmission]:checked').val());
+		itemPrice +=  parseInt($('input[name=package]:checked').val());
+
+		itemPriceHolder.text(numberWithSpaces(itemPrice) + ' руб.');
+	};
+
+	function compileSpecs() {
+		modelSpecs = $('input[name=engine]:checked + label').text();
+		modelSpecs += ', ' + $('input[name=transmission]:checked + label').text();
+		modelSpecs += ', ' + $('input[name=package]:checked + label').text() + '.';
+
+		itemSpecsHolder.text(modelSpecs);
+	};
+
+	$('.b-1 input').on('change', function() {
+		compileSpecs();
+		calculatePrice();
+	});
+
+	compileSpecs();
+	calculatePrice();
 });
